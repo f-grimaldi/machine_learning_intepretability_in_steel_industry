4054005e3c0a0152c5c0b025abd134606dc398d4
fgrim <f.grimaldi.94@gmail.com>
2020-09-02 16:42:01 +0200

    updated src

--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,27 @@ import pandas as pd
 import shutil
 import os
 from sklearn.utils import shuffle
-import src.dataset as dataset
+try:
+    import src.dataset as dataset
+except:
+    import dataset
 from torchvision import transforms
+
+def mask_metrics(m_true, mask, cutoff=0.7):
+    m_pred = mask.clone()
+    m_pred[m_pred < 0.7] = 0
+    m_pred[m_pred >= 0.7] = 1
+    m_pred = m_pred.cpu().long()
+
+    tp = m_pred[(m_true == 1) & (m_pred == 1)].shape[0]
+    fp = m_pred[(m_true == 0) & (m_pred == 1)].shape[0]
+    tn = m_pred[(m_true == 0) & (m_pred == 0)].shape[0]
+    fn = m_pred[(m_true == 1) & (m_pred == 0)].shape[0]
+
+    return m_pred, {'Accuracy': (tp+tn)/(tp+fp+fn+tn),
+                    'Recall':tp/(tp+fn),
+                    'Dice': tp/(tp+fp)}
+
 
 class SaveOutput:
     def __init__(self):
@@ -28,7 +47,7 @@ class MidpointNormalize(colors.Normalize):
         x, y = [self.vmin, self.vcenter, self.vmax], [self.vmin, self.vcenter, self.vmax]
         return np.ma.masked_array(np.interp(value, x, y))
 
-def train_test_split(metadata_root, val_size, shuffle_data=False):
+def train_val_split(metadata_root, val_size, shuffle_data=False):
     df = pd.read_csv(metadata_root)
     val_index = np.random.choice(np.arange(0, df.shape[0]), int(val_size*df.shape[0]), replace=False)
     df_train = df.iloc[df.index.isin(val_index) == False, :]
@@ -36,6 +55,18 @@ def train_test_split(metadata_root, val_size, shuffle_data=False):
     if shuffle_data:
         return shuffle(df_train), shuffle(df_val)
     return df_train, df_val
+
+def train_val_test_split(df, val_size, test_size, shuffle_data=True):
+    val_index = np.random.choice(np.arange(0, df.shape[0]), int(val_size*df.shape[0]), replace=False)
+    df_train = df.iloc[df.index.isin(val_index) == False, :]
+    df_val_test = df.iloc[val_index, :]
+    test_index = np.random.choice(df_val_test.index, int(test_size*df_val_test.shape[0]), replace=False)
+
+    df_val = df_val_test.iloc[df_val_test.index.isin(test_index) == False, :]
+    df_test = df_val_test.iloc[df_val_test.index.isin(test_index), :]
+    if shuffle_data:
+        return shuffle(df_train), shuffle(df_val), shuffle(df_test)
+    return df_train, df_val, df_test
 
 ### Get unqiue metadata
 def get_unique_metadata(METADATA_PATH, max_imgs=40):
@@ -70,30 +101,6 @@ def get_default_params():
               'std': [0.229, 0.224, 0.225],
               'colors': [[0, 0.25, 0.25], [0.25, 0.0, 0.0], [0.0, 0.25, 0.0], [0.0, 0.0, 0.25]]}
     return params
-
-def get_transform(size, mean, std):
-    img_transform = transforms.Compose([transforms.Resize(size = size), transforms.ToTensor(),
-                                      transforms.Normalize(mean = mean, std = std)])
-    mask_transform = transforms.Compose([dataset.Resize(size = (size[1], size[0]))])
-    return img_transform, mask_transform
-
-def create_data(images_path, metadata_path,
-                batch_size=1000, num_workers= 0,
-                return_mask=True, params=None):
-    if not params:
-        params = get_default_params()
-        print('Loading default params:\n{}'.format(params))
-    img_t, mask_t = get_transform(params['size'], params['mean'], params['std'])
-    data = dataset.SteelDataset(metadata_root= metadata_path,
-                                image_root = images_path,
-                                transform = img_t,
-                                mask_transform = mask_t)
-    matrix_generator = dataset.SteelMatrix(SteelDataset=data,
-                                           batch_size=batch_size,
-                                           n_workers=num_workers)
-    X, M, y = matrix_generator.get_matrix(return_mask = return_mask)
-    return X, y, M
-
 
 def vgg_load_state_dict(net, JSON_PATH, verbose=0):
     ### 4.2 Load Parameters
